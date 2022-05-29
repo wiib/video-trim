80aaa4e2cbe7bd00babe45c56161c3b6cc710cfc
wiib <16376176+wiib@users.noreply.github.com>
2022-05-29 15:37:56 -0500

    Set up for deployment
    * Add deploy script via Vite's documentation
    * Implement coi-serviceworker for SharedArrayBuffer support on gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,6 @@ export default defineConfig({
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin"
     }
-  }
+  },
+  base: '/video-trim/'
 });
